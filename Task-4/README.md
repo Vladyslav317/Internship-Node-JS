4d8d83f4a4877cd2428c1be6eaedd2a0b37c6b29
Vladyslav <vladyslav3177@gmail.com>
2022-07-19 19:36:46 +0300

    Solution

---
+++ Task-4/README.md
@@ -0,0 +1,15 @@
+# What is this?
+
+```
+
+  This code creates a pdf file from the new found page and a png screenshot with changed navbar background color.
+
+```
+
+# How to use?
+
+```
+
+  In order to run the code, just simply go to the src folder and type `node index.js` in the console command.
+
+```
---
+++ Task-4/src/index.js
@@ -0,0 +1,43 @@
+const puppeteer = require('puppeteer');
+
+async function getNewPage() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  try {
+    await page.goto('https://pptr.dev/');
+    await page.type('.navbar__search-input', 'pdf');
+    await page.waitForSelector('.navbar__search-input');
+    await page.keyboard.press('Enter');
+    await page.waitForSelector('img');
+    await page.pdf({ path: 'newPage.pdf', format: 'A4' });
+
+    await browser.close();
+  } catch (error) {
+    await browser.close();
+  } finally {
+    await browser.close();
+  }
+}
+
+async function changeStyle() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  try {
+    await page.goto('https://pptr.dev/');
+    await page.evaluate(() => {
+      document.querySelector('.navbar').style.backgroundColor = '#1f54c0';
+    });
+
+    await page.screenshot({ path: 'changeColor.png' });
+
+    await browser.close();
+  } catch (error) {
+    await browser.close();
+  } finally {
+    await browser.close();
+  }
+}
+
+getNewPage();
+changeStyle();
